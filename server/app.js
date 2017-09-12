f4aab4114902538396e8014e0cf4150c1598531b
denis-onsare <denisonsare@gmail.com>
2017-09-12 23:55:18 +0300

    Refactor: client, Adds services to interface with the backend

--- server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('API running')
-})
-
-app.listen(3000, () => console.log('App listening on port http://localhost:3000'))
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,22 @@
+// initialize dependencies by requiring them
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// create instance of express application
 const app = express()
 
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('API running')
+  res.json({'message': 'welcome to the api'})
+})
+
+app.post('/register', (req, res) => {
+  res.json({message: `hello ${req.body.email} you have successfully registered`})
 })
 
 app.listen(3000, () => console.log('App listening on port http://localhost:3000'))
