a32174ea3ff0274e338085d5ee696f77ee970547
denis-onsare <denisonsare@gmail.com>
2017-09-12 20:05:10 +0300

    Setup express

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send("API running")
+})
+
+app.listen(3000, () => console.log("App listening on port http://localhost:3000"))
